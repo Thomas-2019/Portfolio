1eb8db0883814486d24d2e4717d4ddce57944232
Thomas-2019 <a_s1014@yahoo.com.tw>
2019-12-26 15:20:25 +0800

    finsh-v1.1

--- Thomas Portfolio | FrontEnd Development v1.1/js/all.js
+++ Thomas Portfolio | FrontEnd Development v1.1/js/all.js
@@ -1,22 +1,22 @@
 /* fade out*/
-// (function() {
-//   "use strict";
-//   function fadeOut(el) {
-//     el.style.opacity = 1;
-//     (function fade() {
-//       if ((el.style.opacity -= 0.1) < 0) {
-//         el.style.display = "none";
-//       } else {
-//         requestAnimationFrame(fade);
-//       }
-//     })();
-//   }
+(function() {
+  "use strict";
+  function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+      if ((el.style.opacity -= 0.1) < 0) {
+        el.style.display = "none";
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
 
-//   window.addEventListener("load", function() {
-//     const loaderWrapper = document.querySelector(".loader");
-//     fadeOut(loaderWrapper);
-//   });
-// })();
+  window.addEventListener("load", function() {
+    const loaderWrapper = document.querySelector(".loader");
+    fadeOut(loaderWrapper);
+  });
+})();
 
 // // Scroll to specific values
 // (function() {
